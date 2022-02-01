12ce767313509fb0c24dda1980c0911fbdd6e0cb
Michael Hamilton <hmisaiah30@gmail.com>
2022-02-01 02:17:42 -0700

    other projects live on click

--- content.js
+++ content.js
@@ -1,13 +1,28 @@
-const projectName = document.querySelector('#project');
-const tech = document.querySelector('#tech');
-const desc = document.querySelector('#desc');
-
-
 var obj = localStorage.getItem("project");
 obj = JSON.parse(obj);
 
-const { title, description, stack, github, site } = obj;
+var zero = localStorage.getItem("zero");
+zero = JSON.parse(zero);
 
-projectName.innerText = title;
-tech.innerText = stack;
-desc.innerText = description;
+var one = localStorage.getItem("one");
+one = JSON.parse(one);
+
+const { title, description, stack, github, site, gif } = obj;
+
+const projectName = document.querySelector('#project').textContent = title;
+const tech = document.querySelector('#tech').textContent = stack;
+const desc = document.querySelector('#desc').textContent = description;
+const iframe = document.querySelector('iframe').src = gif;
+const git = document.querySelector('#git').href = github;
+const live = document.querySelector('#site').href = site;
+
+const zeroDiv = document.querySelector('.zero');
+zeroDiv.querySelector('p').textContent = zero.title;
+zeroDiv.querySelectorAll('button a')[1].href = zero.site;
+
+const oneDiv = document.querySelector('.one');
+oneDiv.querySelector('p').textContent = one.title;
+oneDiv.querySelectorAll('button a')[1].href = one.site;
+
+const twoDiv = document.querySelector('.two');
+twoDiv.querySelector('p').textContent = 'Coming Soon!';
--- index.js
+++ index.js
@@ -6,30 +6,35 @@ const projectArray = [
         description: '',
         stack: 'MongoDB | Express.js | Node.js | React.js',
         github: 'https://github.com/hamilton-mike/Marvel-Backend',
-        site: 'https://shield-commander.netlify.app/'
+        site: 'https://shield-commander.netlify.app/',
+        gif: 'https://giphy.com/embed/k44WnHDdvfClW'
     },
     {
         title: 'Tamagotchi',
         description: '',
         stack: 'HTML | CSS | Javascript',
         github: 'https://github.com/hamilton-mike/vegetaTamagotchi',
-        site: 'https://vegetatamagotchi.netlify.app/'
+        site: 'https://vegetatamagotchi.netlify.app/',
+        gif: 'https://giphy.com/embed/XAr3mee7JuXYc'
     },
     {
         title: 'Galaxy Blog',
         description: '',
         stack: 'HTML | CSS | EJS | MongoDB',
-        github: 'https://github.com/hamilton-mike/project_One',
-        site: 'https://ga-one-project.herokuapp.com/registration'
+        github: 'https://github.com/hamilton-mike/project_One/tree/mike1',
+        site: 'https://ga-one-project.herokuapp.com/registration',
+        gif: 'https://giphy.com/embed/9E7kUhnT9eDok'
     }
 ];
-
 
 for (let i = 0; i < projects.length; i++) {
     let project = projects[i];
 
     project.childNodes[3].addEventListener('click', () => {
-        localStorage.setItem("project", JSON.stringify(projectArray[i]))
-        location.href = 'project-info.html'
-    })
+        localStorage.setItem("project", JSON.stringify(projectArray[i]));
+        const others = projectArray.filter(obj => obj !== projectArray[i]);
+        localStorage.setItem("zero", JSON.stringify(others[0]));
+        localStorage.setItem("one", JSON.stringify(others[1]));
+        location.href = 'project-info.html';
+    });
 }
